478cc3405a2dd9d6daa2e6c88572125413bb5efc
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-01-28 18:22:13 +0600

    added PostManager class

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] post;
+
+ public Post[] searchPostInWall(Post[] post) {
+     return post;
+
+ }
+    public void deletePostInWall(Post[] post) {
+        return;
+
+    }
+}
